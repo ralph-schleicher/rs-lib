f1dfff6a38f581e6c41a9d30bfb7c046ec0c76b6
Ralph Schleicher <rs@ralph-schleicher.de>
2014-05-30 19:44:28 +0000

    Initial import.

---
+++ rs-sort.h
@@ -0,0 +1,61 @@
+/* rs-sort.h --- searching and sorting.
+
+   Copyright (C) 1993 Ralph Schleicher
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in
+        the documentation and/or other materials provided with the
+        distribution.
+
+      * The name of the author may not be used to endorse or promote
+        products derived from this software without specific prior
+        written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS
+   OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+   INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+   STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+   IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.  */
+
+
+#ifndef RS_SORT_H
+#define RS_SORT_H
+
+#ifdef __cplusplus
+#define RS_SORT_BEGIN_DECL extern "C" {
+#define RS_SORT_END_DECL }
+#else /* not __cplusplus */
+#define RS_SORT_BEGIN_DECL
+#define RS_SORT_END_DECL
+#endif /* not __cplusplus */
+
+RS_SORT_BEGIN_DECL
+
+extern int rs_compare_int (void const *__a, void const *__b);
+extern int rs_compare_unsigned_int (void const *__a, void const *__b);
+extern int rs_compare_long (void const *__a, void const *__b);
+extern int rs_compare_unsigned_long (void const *__a, void const *__b);
+extern int rs_compare_short (void const *__a, void const *__b);
+extern int rs_compare_unsigned_short (void const *__a, void const *__b);
+extern int rs_compare_char (void const *__a, void const *__b);
+extern int rs_compare_unsigned_char (void const *__a, void const *__b);
+extern int rs_compare_signed_char (void const *__a, void const *__b);
+extern int rs_compare_float (void const *__a, void const *__b);
+extern int rs_compare_double (void const *__a, void const *__b);
+
+RS_SORT_END_DECL
+
+#endif /* not RS_SORT_H */
