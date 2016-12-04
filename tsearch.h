6e85646310901c6bbabcb497a419d9021a2da896
Ralph Schleicher <rs@ralph-schleicher.de>
2016-12-04 13:10:04 +0000

    Add file.
    git-svn-id: https://svn/repo/rs-lib/trunk@34 12250caf-61d9-4a4f-88eb-f3065243f0ff

---
+++ tsearch.h
@@ -0,0 +1,64 @@
+/* tsearch.h --- red/black search trees.
+
+   Copyright (C) 2008 Ralph Schleicher
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in
+        the documentation and/or other materials provided with the
+        distribution.
+
+      * Neither the name of the copyright holder nor the names of its
+        contributors may be used to endorse or promote products derived
+        from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+   FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+   COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.  */
+
+#ifndef TSEARCH_H
+#define TSEARCH_H
+
+#ifdef __cplusplus
+#define TSEARCH_BEGIN_DECL extern "C" {
+#define TSEARCH_END_DECL }
+#else
+#define TSEARCH_BEGIN_DECL
+#define TSEARCH_END_DECL
+#endif
+
+TSEARCH_BEGIN_DECL
+
+typedef enum
+  {
+    preorder,
+    postorder,
+    endorder,
+    leaf
+  }
+VISIT;
+
+extern void *tsearch (void const *__key, void **__vrootp, int (*__compar) (void const *, void const *));
+extern void *tfind (void const *__key, void *const *__vrootp, int (*__compar) (void const *, void const *));
+extern void *tdelete (void const *__key, void **__vrootp, int (*__compar) (void const *, void const *));
+extern void twalk (void const *__vroot, void (*__action) (void const *, VISIT, int));
+extern void tdestroy (void *__vroot, void (*__freefct) (void *));
+
+TSEARCH_END_DECL
+
+#endif /* TSEARCH_H */
